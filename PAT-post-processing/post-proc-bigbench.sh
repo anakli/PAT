c7ae0642d778a634812b4a83cb71bd0607799018
Ana Klimovic <anakli@stanford.edu>
2017-09-24 10:13:29 -0700

    post-proc-bigbench.sh update

--- PAT-post-processing/post-proc-bigbench.sh
+++ PAT-post-processing/post-proc-bigbench.sh
@@ -11,9 +11,11 @@ fi
 
 query_name=$1
 
-mkdir -p /home/ubuntu/PAT/PAT-collecting-data/results/$query_name
-mkdir -p /home/ubuntu/PAT/PAT-collecting-data/results/$query_name/pdf
-mkdir -p /home/ubuntu/PAT/PAT-collecting-data/results/$query_name/disk_stats
+mkdir -p ~/PAT/PAT-collecting-data/results/bb-$query_name
+mkdir -p ~/PAT/PAT-collecting-data/results/bb-$query_name/pdf
+mkdir -p ~/PAT/PAT-collecting-data/results/bb-$query_name/disk_stats
+mkdir -p ~/PAT/PAT-collecting-data/results/bb-$query_name/perf
+mkdir -p ~/PAT/PAT-collecting-data/results/bb-$query_name/cpu_stats
 
 
 for test_name in bigbench-$query_name-*
@@ -44,15 +46,18 @@ do
 	fi
 
 	echo "disk1: $disk1, disk2: $disk2"
-	sed -i "97s/if \"[a-z 0-9]*\" in/if \"${disk1}\" in/" disk_module.py
-	sed -i "105s/elif \"[a-z 0-9]*\" in/elif \"${disk2}\" in/" disk_module.py
+	sed -i "96s/if \"[a-z 0-9]*\" in/if \"${disk1}\" in/" disk_module.py
+	sed -i "103s/elif \"[a-z 0-9]*\" in/elif \"${disk2}\" in/" disk_module.py
 
 	sed -i "55s/.*/\t<source>\/home\/ubuntu\/PAT\/PAT-collecting-data\/results\/${test_name}\/instruments<\/source>/" config.xml
 	./pat-post-process.py
-	cp /home/ubuntu/PAT/PAT-collecting-data/results/$test_name/instruments/PAT-Result.pdf /home/ubuntu/PAT/PAT-collecting-data/results/$test_name/instruments/PAT-$test_name-postproc.pdf
-	cp /home/ubuntu/PAT/PAT-collecting-data/results/$test_name/instruments/disk_avg_stats.csv /home/ubuntu/PAT/PAT-collecting-data/results/$test_name/instruments/disk-avg-stats-$test_name.csv
-	cp /home/ubuntu/PAT/PAT-collecting-data/results/$test_name/instruments/PAT-Result.pdf /home/ubuntu/PAT/PAT-collecting-data/results/$query_name/pdf/PAT-$test_name-postproc.pdf
-	cp /home/ubuntu/PAT/PAT-collecting-data/results/$test_name/instruments/disk_avg_stats.csv /home/ubuntu/PAT/PAT-collecting-data/results/$query_name/disk_stats/disk-avg-stats-$test_name.csv
+	cp ~/PAT/PAT-collecting-data/results/$test_name/instruments/PAT-Result.pdf ~/PAT/PAT-collecting-data/results/$test_name/instruments/PAT-$test_name-postproc.pdf
+	cp ~/PAT/PAT-collecting-data/results/$test_name/instruments/disk_avg_stats.csv ~/PAT/PAT-collecting-data/results/$test_name/instruments/disk-avg-stats-$test_name.csv
+	cp ~/PAT/PAT-collecting-data/results/$test_name/instruments/perf.csv ~/PAT/PAT-collecting-data/results/$test_name/instruments/perf-$test_name.csv
+	cp ~/PAT/PAT-collecting-data/results/$test_name/instruments/PAT-Result.pdf ~/PAT/PAT-collecting-data/results/bb-$query_name/pdf/PAT-$test_name-postproc.pdf
+	cp ~/PAT/PAT-collecting-data/results/$test_name/instruments/disk_avg_stats.csv ~/PAT/PAT-collecting-data/results/bb-$query_name/disk_stats/disk-avg-stats-$test_name.csv
+	cp ~/PAT/PAT-collecting-data/results/$test_name/instruments/perf.csv ~/PAT/PAT-collecting-data/results/bb-$query_name/perf/perf-$test_name.csv
+	cp ~/PAT/PAT-collecting-data/results/$test_name/instruments/cpu_stats.csv ~/PAT/PAT-collecting-data/results/bb-$query_name/cpu_stats/cpu-$test_name.csv
 
 	#break
 	cd ~/PAT/PAT-collecting-data/results
