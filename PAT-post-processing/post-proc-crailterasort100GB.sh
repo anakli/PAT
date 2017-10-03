0292cb6ece8d8e17120266128cebb5cd7fab952b
Ana Klimovic <ana.klimov@gmail.com>
2017-10-03 01:58:12 +0000

    update post proc scripts

--- PAT-post-processing/post-proc-crailterasort100GB.sh
+++ PAT-post-processing/post-proc-crailterasort100GB.sh
@@ -10,7 +10,7 @@ mkdir -p ~/PAT/PAT-collecting-data/results/crailterasort100GB/cpu_stats
 
 
 #for test_name in bigbench-$query_name-*
-for test_name in crailterasort100g*i3.xl*
+for test_name in crailterasort100g*
 #for test_name in vanilla-spark-i3.xl-8node-HDD500GB-nvme-crailterasort100GB-8exec-4cores-24GBexecmem
 do
 	echo $test_name
@@ -59,54 +59,5 @@ do
 	cd ~/PAT/PAT-collecting-data/results
 done
 
-
-for test_name in crailterasort100g*i3.4xl*
-#for test_name in vanilla-spark-i3.xl-8node-HDD500GB-nvme-crailterasort100GB-8exec-4cores-24GBexecmem
-do
-	echo $test_name
-	cd ~/PAT/PAT-post-processing
-	if [[ ($test_name == *"HDD"*) || ($test_name == *"hdd"*) || ($test_name == *"hhd"*)  ]] ; then
-		disk1="xvdd"
-		#if [[ (($test_name == *"HDD"*) && ($test_name == *"SSD"*) || ($test_name == *"hdd"*) && ($test_name == *"ssd"*))  ]]; then
-		if [[ ($test_name == *"SSD"*) || ($test_name == *"ssd"*)  ]]; then
-			disk2="xvde"
-		elif [[ ($test_name == *"NVMe"*) || ($test_name == *"nvme"*)  ]]; then
-			disk2="nvme"
-		else
-			disk2="xvdd"
-			#continue
-		fi
-	elif [[ ($test_name == *"SSD"*) || ($test_name == *"ssd"*) ]] ; then
-		disk1=xvde
-		if [[ ($test_name == *"NVMe"*) || ($test_name == *"nvme"*)  ]]; then
-			disk2="nvme"
-		else
-			disk2="xvde"
-			#continue
-		fi
-	else
-		#continue
-		disk1="nvme"
-		disk2="nvme"
-	fi
-
-	echo "disk1: $disk1, disk2: $disk2"
-	sed -i "96s/if \"[a-z 0-9]*\" in/if \"${disk1}\" in/" disk_module.py
-	sed -i "103s/elif \"[a-z 0-9]*\" in/elif \"${disk2}\" in/" disk_module.py
-
-	#sed -i "55s/.*/\t<source>\/home\/ubuntu\/PAT\/PAT-collecting-data\/results\/${test_name}\/instruments<\/source>/" config.xml
-	sed -i "55s/.*/\t<source>\/home\/ubuntu\/PAT\/PAT-collecting-data\/results\/${test_name}\/instruments<\/source>/" config.xml
-	./pat-post-process.py
-	cp ~/PAT/PAT-collecting-data/results/$test_name/instruments/PAT-Result.pdf /home/ubuntu/PAT/PAT-collecting-data/results/$test_name/instruments/PAT-$test_name-postproc.pdf
-	cp ~/PAT/PAT-collecting-data/results/$test_name/instruments/disk_avg_stats.csv /home/ubuntu/PAT/PAT-collecting-data/results/$test_name/instruments/disk-avg-stats-$test_name.csv
-	cp ~/PAT/PAT-collecting-data/results/$test_name/instruments/perf.csv ~/PAT/PAT-collecting-data/results/$test_name/instruments/perf-$test_name.csv
-	cp ~/PAT/PAT-collecting-data/results/$test_name/instruments/PAT-Result.pdf /home/ubuntu/PAT/PAT-collecting-data/results/crailterasort100GB/pdf/PAT-$test_name-postproc.pdf
-	cp ~/PAT/PAT-collecting-data/results/$test_name/instruments/disk_avg_stats.csv /home/ubuntu/PAT/PAT-collecting-data/results/crailterasort100GB/disk_stats/disk-avg-stats-$test_name.csv
-	cp ~/PAT/PAT-collecting-data/results/$test_name/instruments/perf.csv ~/PAT/PAT-collecting-data/results/crailterasort100GB/perf/perf-$test_name.csv
-	cp ~/PAT/PAT-collecting-data/results/$test_name/instruments/cpu_stats.csv ~/PAT/PAT-collecting-data/results/crailterasort100GB/cpu_stats/cpu-$test_name.csv
-
-	#break
-	cd ~/PAT/PAT-collecting-data/results
-done
 
 #cd ~/PAT/PAT-collecting-data
